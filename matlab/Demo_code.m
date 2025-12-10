ae8b2500b6ffa954d548cd841df19609dd46b803
Zhenyang Xiao <zhenyangxiao@utexas.edu>
2025-12-10 10:15:55 -0600

    demo code fix
    comment out the GSV related lines

--- matlab/Demo_code.m
+++ matlab/Demo_code.m
@@ -5,10 +5,10 @@ td=load('example data\TDS_data.mat');
 tdn=td.tdn;
 fc=4.5;
 [Vf,info] = Rubber_Band_Filter(tdn(:,1),tdn(:,2),fc,'tol',1e-11,'niter',50);
-[Vf1,info1] = Rubber_Band_Filter_GCV(tdn(:,1),tdn(:,2),fc,'tol',1e-11,'niter',50);
+% [Vf1,info1] = Rubber_Band_Filter_GCV(tdn(:,1),tdn(:,2),fc,'tol',1e-11,'niter',50);
 %% make plots
 figure;
 plot(tdn(:,1),tdn(:,2)); hold on
 plot(tdn(:,1),Vf);
-plot(tdn(:,1),Vf1);
-legend('Initial','Filtered','GCV')
+% plot(tdn(:,1),Vf1);
+legend('Initial','Filtered')
--- python/demo_code.py
+++ python/demo_code.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import scipy.io as sio
 #import numpy as np
 from rubber_band_filter import rubber_band_filter 
-from rubber_band_filter_gcv import rubber_band_filter_gcv 
+#from rubber_band_filter_gcv import rubber_band_filter_gcv 
 # # fake data
 # np.random.seed(0) 
 # ts = np.linspace(0, 1, 5000)
@@ -20,11 +20,11 @@ ts, Vs = tdn[:,0].astype(float), tdn[:,1].astype(float)
 fc = 4.5  # THz
 
 Vf, info = rubber_band_filter(ts, Vs, fc,'tol', 1e-11,'niter',50)  # default behavior
-Vf1, info1 = rubber_band_filter_gcv(ts, Vs, fc,'tol', 1e-11,'niter',50)  # default behavior
+#Vf1, info1 = rubber_band_filter_gcv(ts, Vs, fc,'tol', 1e-11,'niter',50)  # default behavior
 print(info['iterations_used'], info['dv'])
-print(info1['iterations_used'], info1['dv'])
+#print(info1['iterations_used'], info1['dv'])
 
 plt.plot(ts, Vs, label='Original')
 plt.plot(ts, Vf, label='Filtered')
-plt.plot(ts, Vf1, label='GCV')
+#plt.plot(ts, Vf1, label='GCV')
 plt.legend(); plt.show()
