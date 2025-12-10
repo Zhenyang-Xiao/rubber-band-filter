969a8687bc33ac3edd16eb19f55dde4d15c05a55
Zhenyang Xiao <zhenyangxiao@utexas.edu>
2025-12-10 12:34:29 -0600

    Changed the data used in the demo code

--- matlab/Demo_code.m
+++ matlab/Demo_code.m
@@ -1,14 +1,16 @@
 clearvars;
 %% clear path
 restoredefaultpath;
-td=load('example data\TDS_data.mat');
-tdn=td.tdn;
-fc=4.5;
-[Vf,info] = Rubber_Band_Filter(tdn(:,1),tdn(:,2),fc,'tol',1e-11,'niter',50);
+rng(0);
+dt = .01;
+t = [0:dt:10]';
+V = 2*t + randn(size(t))*0.2 + sin(2*pi*1.5*t+3*pi/8);
+fc = 2;
+[Vf,info] = Rubber_Band_Filter(t,V,fc,'tol',1e-7,'niter',80);
 % [Vf1,info1] = Rubber_Band_Filter_GCV(tdn(:,1),tdn(:,2),fc,'tol',1e-11,'niter',50);
 %% make plots
 figure;
-plot(tdn(:,1),tdn(:,2)); hold on
-plot(tdn(:,1),Vf);
+plot(t,V); hold on
+plot(t,Vf);
 % plot(tdn(:,1),Vf1);
 legend('Initial','Filtered')
--- python/demo_code.py
+++ python/demo_code.py
@@ -5,26 +5,22 @@ Created on Tue Sep 23 10:17:22 2025
 @author: xiaoz
 """
 import matplotlib.pyplot as plt
-import scipy.io as sio
-#import numpy as np
+#import scipy.io as sio
+import numpy as np
 from rubber_band_filter import rubber_band_filter 
 #from rubber_band_filter_gcv import rubber_band_filter_gcv 
 # # fake data
-# np.random.seed(0) 
-# ts = np.linspace(0, 1, 5000)
-# Vs = np.cos(2*np.pi*20*ts) + 0.2*np.random.randn(ts.size)
-# fc = 30.0  # Hz
-# real data
-tdn = sio.loadmat("example data/TDS_data.mat")["tdn"]   # shape (N,2)
-ts, Vs = tdn[:,0].astype(float), tdn[:,1].astype(float)
-fc = 4.5  # THz
+np.random.seed(0) 
+dt = 0.01
+t = np.arange(0, 10 + dt, dt)[:, None]   
+V = 2 * t + np.random.randn(*t.shape) * 0.2 + np.sin(2 * np.pi * 1.5 * t + 3 * np.pi / 8)
+fc = 2
 
-Vf, info = rubber_band_filter(ts, Vs, fc,'tol', 1e-11,'niter',50)  # default behavior
-#Vf1, info1 = rubber_band_filter_gcv(ts, Vs, fc,'tol', 1e-11,'niter',50)  # default behavior
+Vf, info = rubber_band_filter(t, V, fc,'tol', 1e-7,'niter',80)  
 print(info['iterations_used'], info['dv'])
 #print(info1['iterations_used'], info1['dv'])
 
-plt.plot(ts, Vs, label='Original')
-plt.plot(ts, Vf, label='Filtered')
+plt.plot(t, V, label='Original')
+plt.plot(t, Vf, label='Filtered')
 #plt.plot(ts, Vf1, label='GCV')
 plt.legend(); plt.show()
